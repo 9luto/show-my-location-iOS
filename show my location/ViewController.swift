23507727bc60c623b65113b0c3383a948f5498b1
Richard Peralta <peraltar91@my.sunyulster.edu>
2020-05-10 16:24:25 -0400

    Initial Commit

---
+++ show my location/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  show my location
+//
+//  Created by Richard Peralta on 5/10/20.
+//  Copyright © 2020 Richard Peralta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
